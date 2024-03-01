d9baa8ed83246a9e0533ce8f997444b420b1630a
meheraz1100 <meheraz1100@gmail.com>
2024-03-01 07:00:53 +0600

    added use of backticks

---
+++ template.js
@@ -0,0 +1,25 @@
+const first = 'Jaan';
+const last = 'pakhi';
+const greet = 'Hello';
+const fullName = first +' ' + last +' ' + greet;
+console.log(fullName);
+
+
+//using single quotes
+const a = 10;
+const b = 20;
+const result = 'The sum of ' + a + ' and '+ b +' is ' + (a + b);
+console.log(result);
+
+// using backticks
+const numbers = [45, 87, 12, 34];
+const math = `The sum of ${a} and ${b} is ${a + b}`
+console.log(math);
+
+
+// using backticks 
+const email = `Hi jhon     
+cena cena lage
+tumi mara mari korba naki`;
+console.log(email);
+
